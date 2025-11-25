b9b388b9e57944245535dc78724bd07acc6053e5
Faizal <nightowlclub72@gmail.com>
2025-11-25 09:52:20 +0530

    Update: Fix server port and add package.json

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
+import cors from "cors"
 import OpenAI from "openai"
 
 const app = express();
 const openai = new OpenAI({apiKey: process.env.OPENAI});
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
 
